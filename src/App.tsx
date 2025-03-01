57e9da35ba7c3bf75bc0be21b6958637b6d520e3
Ivansa71 <ithuta484@gmail.com>
2025-03-01 21:24:54 +0300

    commit1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { HW1 } from './HW/1/HW1.tsx';
+import { HW2 } from './HW/2/HW2.tsx';
+import { HW3 } from './HW/3/HW3.tsx';
+import { HW4 } from './HW/4/HW4.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <HW1 />
+      {/*<HW2 />*/}
+      {/*<HW3 />*/}
+      {/*<HW4 />*/}
+    </div>
+  );
+}
+
+export default App;
