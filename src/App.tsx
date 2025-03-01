f55abf217c96a721babd6b5f0f97d033d1fcc9f3
Ivansa71 <ithuta484@gmail.com>
2025-03-02 00:21:52 +0300

    commit1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { HW1 } from './HW/1/HW1.tsx';
-import { HW2 } from './HW/2/HW2.tsx';
-import { HW3 } from './HW/3/HW3.tsx';
-import { HW4 } from './HW/4/HW4.tsx';
+// import { HW2 } from './HW/2/HW2.tsx';
+// import { HW3 } from './HW/3/HW3.tsx';
+// import { HW4 } from './HW/4/HW4.tsx';
 
 function App() {
   return (
